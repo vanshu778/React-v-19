9e10851677e4649abd081a1618bb3062e8262540
vanshu778 <vthesiya778@gmail.com>
2025-03-02 00:19:37 +0530

    jsx logic

--- reactv-19/src/App.jsx
+++ reactv-19/src/App.jsx
@@ -1,3 +1,14 @@
 export const App = () => {
-  return <h1>Hello, Vanshika</h1>
+  return (
+    <div>
+      <div>
+        <img src="qot.jpg" alt="qot.jpg" width="50%" height="40%" />
+      </div>
+      <h2>Name: Queen of Tears</h2>
+      <h3>Rating:8.2</h3>
+      <p>
+        Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem doloribus ipsum ex? Sed facere earum eligendi pariatur nemo minima ab quam corporis in, possimus sequi at accusantium voluptates ea ratione!
+      </p>
+    </div>
+  )
 }
