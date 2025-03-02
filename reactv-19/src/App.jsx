74522db7098f117f2d4615d973d026bfd729f0e2
vanshu778 <vthesiya778@gmail.com>
2025-03-02 16:17:36 +0530

    Dynamic values

--- reactv-19/src/App.jsx
+++ reactv-19/src/App.jsx
@@ -16,16 +16,22 @@ export const App = () => {
 };
 
 const NetflixSeries = () => {
+  const name = "Queen of Tears";
+  //const rating= "8.2";
+  const summary = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem doloribus ipsum ex? Sed facere earum eligendi pariatur nemo minima ab quam corporis in, possimus sequi at accusantium voluptates ea ratione!";
+  const returnGenre = () =>{
+    const genre = "RomCom";
+    return genre;
+  }
   return (
     <div>
     <div>
       <img src="qot.jpg" alt="qot.jpg" width="50%" height="40%" />
     </div>
-    <h2>Name: Queen of Tears</h2>
-    <h3>Rating:8.2</h3>
-    <p>
-      Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem doloribus ipsum ex? Sed facere earum eligendi pariatur nemo minima ab quam corporis in, possimus sequi at accusantium voluptates ea ratione!
-    </p>
+    <h2>Name: {name}</h2>
+    <h3>Rating:{5+4.2}</h3>
+    <p>Summary: {summary}</p> 
+    <p>Genre: {returnGenre()}</p>
   </div>
   )
 }
