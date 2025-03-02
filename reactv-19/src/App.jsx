2fe534c8b137bb59864a1ae87cd56f5b0ef4452d
vanshu778 <vthesiya778@gmail.com>
2025-03-02 06:01:40 +0530

    how to create component function

--- reactv-19/src/App.jsx
+++ reactv-19/src/App.jsx
@@ -1,14 +1,26 @@
 export const App = () => {
   return (
     <div>
-      <div>
-        <img src="qot.jpg" alt="qot.jpg" width="50%" height="40%" />
-      </div>
-      <h2>Name: Queen of Tears</h2>
-      <h3>Rating:8.2</h3>
-      <p>
-        Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem doloribus ipsum ex? Sed facere earum eligendi pariatur nemo minima ab quam corporis in, possimus sequi at accusantium voluptates ea ratione!
-      </p>
+  <NetflixSeries/>
+  <NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+<NetflixSeries/>
+  </div>
+  )
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
+    <div>
+      <img src="qot.jpg" alt="qot.jpg" width="50%" height="40%" />
     </div>
+    <h2>Name: Queen of Tears</h2>
+    <h3>Rating:8.2</h3>
+    <p>
+      Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem doloribus ipsum ex? Sed facere earum eligendi pariatur nemo minima ab quam corporis in, possimus sequi at accusantium voluptates ea ratione!
+    </p>
+  </div>
   )
 }
