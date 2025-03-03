a96498130f37718e1564e0ecb3c89c228a05765e
vanshu778 <vthesiya778@gmail.com>
2025-03-03 12:12:12 +0530

    Passing JSX as props

--- reactv-19/src/main.jsx
+++ reactv-19/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
 import { Practices } from './Practices.jsx'
+import Profile from './components/Profile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <Practices/> */}
+    <Profile/>
   </StrictMode>,
 )
