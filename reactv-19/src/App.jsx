944420bdd76a587a1385f27e3c95837cba88f182
vanshu778 <vthesiya778@gmail.com>
2025-03-03 08:23:48 +0530

    looping in jsx

--- reactv-19/src/App.jsx
+++ reactv-19/src/App.jsx
@@ -1,18 +1,9 @@
-//import React from "react";
-import NetflixSeries, { Header,Footer} from "./components/NetflixSeries";
+import NetflixSeries from "./components/NetflixSeries";
 
 export const App = () => {
-  // return ([<NetflixSeries key='1'/>,<NetflixSeries key='2'/>]
-  // )
   return (
     <>
-    <Header/>
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <Footer/>
+      <NetflixSeries />  
     </>
   );
 };
