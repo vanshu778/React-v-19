62c8ba4279b2bedd71e2acd7cb34e70eef4d5122
vanshu778 <vthesiya778@gmail.com>
2025-03-08 08:21:29 +0530

    Todo LocalStorage

--- reactv-19/src/projects/Todo/Todo.jsx
+++ reactv-19/src/projects/Todo/Todo.jsx
@@ -3,23 +3,32 @@ import { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { TodoDate } from "./TodoDate";
+import {
+  getLocalStorageTodoData,
+  setLocalStorageTodoData,
+} from "./TodoLocalStorage";
 
 export const Todo = () => {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(() => getLocalStorageTodoData());
 
   const handleFormSubmit = (inputValue) => {
-    const{id,content,checked} = inputValue;
+    const { id, content, checked } = inputValue;
 
     //to check if the imput field is empty or not
     if (!content) return;
 
     //to check if the data is already existing or not
     // if (task.includes(inputValue)) return;
-    const ifTodoContentMatched = task.find((curTask)=>curTask.content === content);
-    if(ifTodoContentMatched) return;
+    const ifTodoContentMatched = task.find(
+      (curTask) => curTask.content === content
+    );
+    if (ifTodoContentMatched) return;
 
-    setTask((prevTask) => [...prevTask, {id, content, checked}]);
+    setTask((prevTask) => [...prevTask, { id, content, checked }]);
   };
+
+  //Add data to localstorage
+  setLocalStorageTodoData(task);
 
   //Delete Functionality
   const handleDeleteTodo = (value) => {
@@ -33,22 +42,22 @@ export const Todo = () => {
   };
 
   //handleCheckedTodo
-  const handleCheckedTodo=(content) => {
+  const handleCheckedTodo = (content) => {
     const updatedTask = task.map((curTask) => {
-        if(curTask.content === content){
-            return{...curTask, checked: !curTask.checked};
-        }else{
-            return curTask;
-        }
+      if (curTask.content === content) {
+        return { ...curTask, checked: !curTask.checked };
+      } else {
+        return curTask;
+      }
     });
     setTask(updatedTask);
-  }
+  };
 
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
-        <TodoDate/>
+        <TodoDate />
       </header>
 
       <TodoForm onAddTodo={handleFormSubmit} />
@@ -71,7 +80,7 @@ export const Todo = () => {
 
       <section>
         <button className="clear-btn" onClick={handleClearTodoData}>
-        Clear
+          Clear
         </button>
       </section>
     </section>
